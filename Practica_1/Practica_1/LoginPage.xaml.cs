7ab2aa63a771a3456112aadd255b929ced978d6b
Matthews Rosario <matthewsrosarioespinal@gmail.com>
2021-08-20 19:17:01 -0400

    Initial commit

---
+++ Practica_1/Practica_1/LoginPage.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Practica_1
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class LoginPage : ContentPage
+    {
+        public LoginPage()
+        {
+            InitializeComponent();
+        }
+
+        private void OnButtonClicked(object sender, EventArgs e)
+        {
+            string validacionNombre = nombreEntry.Text;
+            string validacionContraseña = contraseñaEntry.Text;
+
+            if (string.IsNullOrEmpty(validacionNombre) && string.IsNullOrEmpty(validacionContraseña))
+            {
+                DisplayAlert("Advertencia!!", "Dejó ambos campos vacios.", "Aceptar");
+            }
+            else if (string.IsNullOrEmpty(validacionNombre))
+            {
+                DisplayAlert("Advertencia!!", "Dejó el Nombre vacio.", "Aceptar");
+            }
+            else if (string.IsNullOrEmpty(validacionContraseña))
+            {
+                DisplayAlert("Advertencia!!", "Dejó la contraseña vacia.", "Aceptar");
+            }
+            else
+            {
+                DisplayAlert("Bienvenido", validacionNombre, "Aceptar");
+            }
+        }
+    }
+}
---
+++ readme.md
@@ -0,0 +1,16 @@
+# Evidencia Funcionamiento App
+
+## Pagina de Inicio.
+<img src='SS\Login_Page.jpg'>
+
+## Entrada Usuario.
+<img src='SS\Bienvenida.jpg'>
+
+## Advertencia campo de nombre vacio
+<img src='SS\Nombre_Vacio.jpg'>'
+
+## Advertencia campo de password vacio.
+<img src='SS\Password_Vacia.jpg'>
+
+## Advertencia ambos campos vacios.
+<img src='SS\Ambos_Campos_Vacios.jpg'>
